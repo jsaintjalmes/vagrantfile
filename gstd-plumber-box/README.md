36db9ec46b4b71b60e3514360c47e3dc46c7e2f0
Justine SAINT-JALMES <justine.saint-jalmes@devialet.com>
2023-04-21 18:40:30 +0200

    ADD: gstd practice box

---
+++ gstd-plumber-box/README.md
@@ -0,0 +1,11 @@
+# plumber kit
+
+## setup
+run ```setup.sh``` script from the root directory to install vagrant and its dependencies.
+in gstd-plumber-box folder, run ```vagrant up``` to install the box, ```vagrant ssh``` to run it. 
+the box is configured with a Shared folder.
+
+## documentation
+
+[gstreamer daemon overview and quick start guide](https://developer.ridgerun.com/wiki/index.php/GStreamer_Daemon)
+[gst-plugin-devialet source code and quich start](https://github.com/devialet/gst-plugins-devialet)
---
+++ gstd-plumber-box/vagrant_provision.sh
@@ -0,0 +1,54 @@
+# update
+apt-get update
+apt-get upgrade -y
+
+# install generic packages
+sudo apt-get install -y \
+	vim \
+	git \
+	meson \
+	make
+
+# install gstreamer
+mkdir Developments
+cd Developments
+git clone https://gitlab.freedesktop.org/gstreamer/gstreamer.git
+cd gstreamer
+git checkout 1.20.5
+
+# gst-plugins-devialet
+#git -C subprojects clone git@github.com:devialet/gst-plugins-devialet.git
+#meson setup --wipe builddir -Dcustom_subprojects=gst-plugins-devialet -Dgstreamer:tools=enabled --prefix=/tmp/gst
+#meson compile -C builddir
+#meson install -C builddir
+
+# install gstd dependencies
+sudo apt-get install -y \
+	automake \
+	libtool \
+	pkg-config \
+	libgstreamer1.0-dev \
+	libgstreamer-plugins-base1.0-dev \
+	libglib2.0-dev \
+	libjson-glib-dev \
+	gtk-doc-tools \
+	libreadline-dev \
+	libncursesw5-dev \
+	libdaemon-dev \
+	libjansson-dev \
+	libsoup2.4-dev \
+	python3-pip \
+	libedit-dev 
+
+# build daemon
+cd /home/vagrant/Developments
+git clone https://github.com/RidgeRun/gstd-1.x.git
+cd /home/vagrant/Developments/gstd-1.x
+sudo ./autogen.sh
+sudo ./configure
+sudo make
+
+# install binaries
+# build environment
+#meson build
+#ninja -C build
