d115a52b9d5a797f1d0581ed8df2c9bf9c6b5e49
Justine SAINT-JALMES <justine.saint-jalmes@devialet.com>
2023-04-24 15:21:22 +0200

    FIX: ssh agent forwarding and gst devialet plugins correctly installed

--- gstd-plumber-box/vagrant_provision.sh
+++ gstd-plumber-box/vagrant_provision.sh
@@ -1,3 +1,7 @@
+## gstd pipeline practice box ##
+
+## BASICS ##
+
 # update
 apt-get update
 apt-get upgrade -y
@@ -9,18 +13,33 @@ sudo apt-get install -y \
 	meson \
 	make
 
+# set user jsaintjalmes
+#useradd -m -s /bin/bash -U jsaintjalmes
+#cp -pr /home/vagrant/.ssh /home/jsaintjalmes
+#chown -R jsaintjalmes:jsaintjalmes /home/jsaintjalmes
+#echo "%jsaintjalmes ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/jsaintjalmes
+
+# git ssh forwarding
+#ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+## GSTREAMER INSTALL ##
+
 # install gstreamer
 mkdir Developments
+chown -R vagrant Developments
 cd Developments
 git clone https://gitlab.freedesktop.org/gstreamer/gstreamer.git
+chown -R vagrant gstreamer
 cd gstreamer
 git checkout 1.20.5
 
 # gst-plugins-devialet
-#git -C subprojects clone git@github.com:devialet/gst-plugins-devialet.git
-#meson setup --wipe builddir -Dcustom_subprojects=gst-plugins-devialet -Dgstreamer:tools=enabled --prefix=/tmp/gst
-#meson compile -C builddir
-#meson install -C builddir
+git -C subprojects clone git@github.com:devialet/gst-plugins-devialet.git
+meson setup --wipe builddir -Dcustom_subprojects=gst-plugins-devialet -Dgstreamer:tools=enabled --prefix=/tmp/gst
+meson compile -C builddir
+meson install -C builddir
+
+## GST DAEMON INSTALL ##
 
 # install gstd dependencies
 sudo apt-get install -y \
@@ -40,7 +59,7 @@ sudo apt-get install -y \
 	python3-pip \
 	libedit-dev 
 
-# build daemon
+# install daemon
 cd /home/vagrant/Developments
 git clone https://github.com/RidgeRun/gstd-1.x.git
 cd /home/vagrant/Developments/gstd-1.x
@@ -55,5 +74,5 @@ sudo make install
 sudo meson build
 sudo ninja -C build
 
-# install binaries
+# install binaries!!
 sudo ninja -C build install
